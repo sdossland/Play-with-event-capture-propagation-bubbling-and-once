7713d2fe5d27968daf981876d5127c9541b3f30c
Sophia Dossland <sophia.dossland@gmail.com>
2017-05-16 19:36:29 -0700

    insert images, initiate capture code

--- logic.js
+++ logic.js
@@ -6,11 +6,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const rectangles = document.querySelectorAll('.rectangle');
 
   function emphasis(e) {
-    console.log();
+    console.log(this.classList.value);
+    e.stopPropagation();
   }
 
   rectangles.forEach(rectangle =>
-    rectangle.addEventListener('click', emphasis)
-  )
-
+    rectangle.addEventListener('click', emphasis, { capture: true })
+  );
+  
 });
